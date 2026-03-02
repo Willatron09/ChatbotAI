7ca638c5255d7e3acebbe3ba20198859d02965d9
Willatron09 <will9.driver@icloud.com>
2026-03-02 21:32:43 +0000

    Add files via upload

---
+++ mybot-basic.py
@@ -0,0 +1,46 @@
+﻿#!/usr/bin/env python3
+
+# A basic chatbot design --- a starting point for developing your own chatbot
+
+#######################################################
+#  Initialise AIML agent
+import aiml
+import wikipedia
+# Create a Kernel object. 
+kern = aiml.Kernel()
+kern.setTextEncoding(None)
+kern.bootstrap(learnFiles="mybot-basic.xml")
+
+# Welcome user
+print("Welcome to this chat bot. Please feel free to ask questions from me!")
+
+# Main loop
+while True:
+    #get user input
+    try:
+        userInput = input("> ")
+    except (KeyboardInterrupt, EOFError):
+        print("Bye!")
+        break
+    #pre-process user input and determine response agent (if needed)
+    responseAgent = 'aiml'
+    #activate selected response agent
+    if responseAgent == 'aiml':
+        answer = kern.respond(userInput)
+    #post-process the answer for commands
+    if answer[0] == '#':
+        params = answer[1:].split('$')
+        cmd = int(params[0])
+        if cmd == 0:
+            print(params[1])
+            break
+        elif cmd == 1:
+            try:
+                wSummary = wikipedia.summary(params[1], sentences=3,auto_suggest=True)
+                print(wSummary)
+            except:
+                print("Sorry, I do not know that. Be more specific!")
+        elif cmd == 99:
+            print("I did not get that, please try again.")
+    else:
+        print(answer)
